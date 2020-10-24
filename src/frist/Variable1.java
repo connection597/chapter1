bd87e2559c8cd8f4aad8ca0c9bf94de1a9bfb797
user <user@DESKTOP-KJUO3L6>
2020-10-24 20:06:52 +0900

    level에 변수 10 대입 하기

---
+++ src/frist/Variable1.java
@@ -0,0 +1,11 @@
+package frist;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level; // 정수형 변수를 level로 선언
+		level = 10; // level 변수 값에 10 대입
+		System.out.print(level); // level 값 출력
+	}
+
+}
